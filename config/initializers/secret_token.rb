79b8130498f94758d797e2ec106d867607d5eece
unknown <Martin Cooper@MARCOO2.trant.co.uk>
2012-11-06 14:24:50 +0000

    type in text here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trant::Application.config.secret_token = 'de06f5438fee5f3d8e86a215064215e36a2f196281dc0742c8d6e43da3a1f16576e954aa857a0a19fb3c2f3aca55fca9b1fb11f7cb41f4286d44c2e3ced2a795'
